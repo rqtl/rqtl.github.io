ae7b08c39121b10f7883438fe25ae1dc4c2438e8
Karl Broman <kbroman@gmail.com>
2017-09-11 16:33:08 -0500

    Add dotfunc.R for plot.geno etc removed from R/qtl vs 1.42-1

---
+++ dotfunc.R
@@ -0,0 +1,9 @@
+# functions removed from R/qtl in version 1.42-1
+
+plot.errorlod <- qtl::plotErrorlod
+plot.geno <- qtl::plotGeno
+plot.info <- qtl::plotInfo
+plot.missing <- qtl::plotMissing
+plot.pheno <- qtl::plotPheno
+plot.pxg <- qtl::plotPXG
+plot.rf <- qtl::plotRF
